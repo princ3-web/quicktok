aa6d945f61d133dc68db5906224f918c19f75905
Michal Gornicki <michal.gornicki@ithouse.co>
2023-09-01 17:37:08 +0200

    first commit

--- .history/src/main_20230901173633.jsx
+++ .history/src/main_20230901173633.jsx
@@ -7,6 +7,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
        <HashRouter >
         <App />
-      </HashRouter >
+      </BrowserRouter>
   </React.StrictMode>,
 )
